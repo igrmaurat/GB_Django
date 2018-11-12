03b8f9314e51407e78a5baa84d0522b35daff798
igrmaurat <igrmaurat@mail.ru>
2018-11-12 20:12:54 +0300

    Урок 5 - Первый коммит - статика + изображения

--- server/images/models.py
+++ server/images/models.py
@@ -1,19 +1,12 @@
 from django.db import models
 
 class Image(models.Model):
-<<<<<<< HEAD
-    title = models.CharField(max_length=150)
-    value = models.ImageField(upload_to='images')
-
-    def __str__(self):
-        return self.value.url
-=======
     title = models.CharField(max_length = 150)
     value = models.ImageField(upload_to = "images")
 
-    def __str__(self):
+    @property
+    def url(self):
         return self.value.url
 
-
-
->>>>>>> 070b20a43dd7b4b011c26b3d783235212c88555d
+    def __str__(self):
+        return self.value.url

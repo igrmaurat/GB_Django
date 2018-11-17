fff2e60b713d27dcc5c7adfa66a7a017d89656cc
igrmaurat <igrmaurat@mail.ru>
2018-11-17 22:06:31 +0300

    Урок 5 - Формы create, update и delete

--- server/products/views.py
+++ server/products/views.py
@@ -1,14 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.template import Template, Context
 from django.template.loader import get_template, render_to_string
 from django.http import HttpResponse
+from django.http import Http404
+from django.urls import reverse_lazy
+
 import json
 
 from .models import Product
+from .forms import ProductForm
 
-# Создаем функцию для главной страницы
 
-# Создаем функцию для страницы about
+def product_delete(request, pk):
+
+   obj = get_object_or_404(Product, pk = pk)
+   success_url = reverse_lazy("main:main")
+
+   if request.method == "POST":
+      obj.delete()
+      return redirect(success_url)
+
+   return render(
+      request,
+      'products/delete.html',
+      {'obj': obj }
+   )
+
+
+
+def product_update(request, pk):
+   # try:
+   #    obj = Product.objects.get(pk = pk)
+   #
+   # except Exception as err:
+   #    raise Http404
+   obj = get_object_or_404(Product, pk = pk)
+
+   form = ProductForm(instance = obj)
+   success_url = reverse_lazy("main:main")
+
+   if request.method == "POST":
+
+      form = ProductForm(request.POST,
+                         files = request.FILES,
+                         instance = obj)
+
+      if form.is_valid():
+         form.save()
+         return redirect(success_url)
+
+   return render(
+      request,
+      'products/update.html',
+      {
+         'form': form,
+         'obj': obj
+      }
+   )
+
+
+
+
+def product_create(request):
+   form = ProductForm()
+   success_url = reverse_lazy("main:main")
+
+   if request.method == "POST":
+      form = ProductForm(request.POST)
+      if form.is_valid():
+         form.save()
+         return redirect(success_url)
+
+
+
+   return render(request, 'products/create.html', {'form': form})
+
+
 def catalog(request):
 
    context = {}

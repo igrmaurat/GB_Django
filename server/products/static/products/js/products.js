240abe89ef94a98229401d9512d9681c205ba73e
igrmaurat <igrmaurat@mail.ru>
2018-11-30 12:54:33 +0300

    Ajax

--- server/products/static/products/js/products.js
+++ server/products/static/products/js/products.js
@@ -2,10 +2,8 @@ const renderProduct = ({id, title, image}) => (
   `
     <div class="catalog">
       <img class="product__image" src="${ image }">
-      <span class="product__name">
-          ${ title }
-      </span>
       <a class="product__link" href="/products/${ id }">
+      ${ title }
       </a>
   </div>
   `
